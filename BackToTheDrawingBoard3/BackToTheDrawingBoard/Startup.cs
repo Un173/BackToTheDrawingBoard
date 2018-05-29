768ec7ca4a37c95dfaa776af8221ed3347af4a3d
Un173 <39371138+Un173@users.noreply.github.com>
2018-05-30 02:21:36 +0300

    Add files via upload
    Добавлен кастомный контроллер

---
+++ BackToTheDrawingBoard3/BackToTheDrawingBoard/Startup.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using BackToTheDrawingBoard.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace BackToTheDrawingBoard
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "SimpleWebApp";
+                options.LoginPath = "/";
+                options.Events.OnRedirectToLogin = context =>
+                {
+                    context.Response.StatusCode = 401;
+                    return Task.CompletedTask;
+                };
+            });
+            services.AddIdentity<User, IdentityRole>()
+.AddEntityFrameworkStores<MyDBContext>();
+            services.AddMvc();
+            //var connection = @"Server=(localdb)\mssqllocaldb;Database=MyDB;Trusted_Connection=True;";
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=MyDBAuthFinal;Trusted_Connection=True;";
+            services.AddDbContext<MyDBContext>(options => options.UseSqlServer(connection));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider services)
+        {
+
+            app.UseAuthentication();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+           
+            app.UseMvc();
+            CreateUserRoles(services).Wait();
+        }
+
+        private async Task CreateUserRoles(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+            // Создание ролей администратора и пользователя
+            if (await roleManager.FindByNameAsync("admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("admin"));
+            }
+            if (await roleManager.FindByNameAsync("user") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("user"));
+            }
+            // Создание Администратора
+            string adminEmail = "admin";
+            string adminPassword = "Aa123456!";
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                User admin = new User { Email = adminEmail, UserName = adminEmail };
+                IdentityResult result = await userManager.CreateAsync(admin, adminPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "admin");
+                }
+            }
+            // Создание Пользвователя
+            string userEmail = "user";
+            string userPassword = "Aa123456!";
+            if (await userManager.FindByNameAsync(userEmail) == null)
+            {
+                User user = new User { Email = userEmail, UserName = userEmail };
+                IdentityResult result = await userManager.CreateAsync(user, userPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "user");
+                }
+            }
+        }
+    }
+}
---
+++ BackToTheDrawingBoard3/BackToTheDrawingBoard/wwwroot/LoadUsers.js
@@ -0,0 +1,128 @@
+﻿function LoadUsers() {
+    var logins, userTypes;
+    var request = new XMLHttpRequest();
+    request.open("GET", "api/LoginTables/", false);
+    request.send();
+    logins = JSON.parse(request.responseText);
+    request.open("GET", "api/UserTypeTables/", false);
+    request.send();
+    userTypes = JSON.parse(request.responseText);
+    
+    var x = "";
+    document.getElementById("user").innerHTML = x;
+    for (i in logins) { x += "<tr><td class=\"col-md-2\">" + logins[i].login + "<\/td>" + "<td class=\"col-md-2\">" + Match(logins[i], userTypes) + "<\/td>" + "<td class=\"col-md-2\">" + "<a href=\"\/edit.html?id=" + logins[i].id + "\">Изменить<\/a>" + "<\/td><td> <button onclick=\"Delete("+logins[i].id+");\">Удалить<\/button><\/td><\/tr>";}
+
+    document.getElementById("user").innerHTML += x;
+    
+}
+function Match(user, types)
+{
+    for (var i in types) if (user.userType == types[i].id) return types[i].name;
+}
+function LoadUser() {
+    var params = getQueryVariable("id");
+    var logins, userTypes;
+   
+    var request = new XMLHttpRequest();
+    request.open("GET", "api/LoginTables/"+params, false);
+    request.send();
+    logins = JSON.parse(request.responseText);
+    request.open("GET", "api/UserTypeTables/", false);
+    request.send();
+    userTypes = JSON.parse(request.responseText);
+
+   
+    //x += logins.login + " " + Match(logins, userTypes);
+
+    var x = "";
+    for (var i in userTypes)
+        if (userTypes[i].id != logins.userType) x += "<option>" + userTypes[i].name + "</option>";
+        else x += "<option selected>" + userTypes[i].name + "</option>";
+   
+
+    
+
+
+    document.getElementById("login").value = logins.login;
+    document.getElementById("type").innerHTML = x;
+}
+function CreatePage()
+{
+    var params = getQueryVariable("id");
+    var userTypes;
+
+    var request = new XMLHttpRequest();
+   
+    request.open("GET", "api/UserTypeTables/", false);
+    request.send();
+    userTypes = JSON.parse(request.responseText);
+
+
+    var x = "";
+    for (var i in userTypes)
+      x += "<option>" + userTypes[i].name + "</option>";
+       
+   
+
+
+    document.getElementById("type").innerHTML = x;
+}
+function Put() {
+    var params = getQueryVariable("id");
+    var userTypes;
+
+    var request = new XMLHttpRequest();
+    request.open("GET", "api/UserTypeTables/", false);
+    request.send();
+    userTypes = JSON.parse(request.responseText);
+    var login = document.getElementById("login").value;
+    var e = document.getElementById("type");
+    var type = GetId(e.options[e.selectedIndex].text, userTypes);
+
+ 
+    request.open("PUT", "api/LoginTables/" + params, false);
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send(JSON.stringify({ id: params, login: login, userType: type }));
+    window.location.replace("index.html");
+}
+function Post() {
+    var params = getQueryVariable("id");
+    var userTypes;
+
+    var request = new XMLHttpRequest();
+    request.open("GET", "api/UserTypeTables/", false);
+    request.send();
+    userTypes = JSON.parse(request.responseText);
+    var login = document.getElementById("login").value;
+    var e = document.getElementById("type");
+    var type = GetId(e.options[e.selectedIndex].text, userTypes);
+
+
+    request.open("POST", "api/LoginTables/", false);
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send(JSON.stringify({login: login, userType: type }));
+    window.location.replace("index.html");
+}
+function Delete(id)
+{
+    //var params = getQueryVariable("id");
+ 
+    var request = new XMLHttpRequest();
+    request.open("DELETE", "api/LoginTables/" + id, false);
+    request.send();
+   // window.location.replace("index.html");
+}
+
+function GetId(type, types) {
+    for (var i in types) if (type == types[i].name) return types[i].id;
+}
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+    return (false);
+}
